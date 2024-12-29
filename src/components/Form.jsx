46eb3a081e923a13462da4414b57c777511b66fc
Yash Bisht <yashbisht2005@gmail.com>
2024-12-29 14:56:09 +0530

    final commits

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
-import { useURLposition } from "../hooks/useURLposition";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useURLposition } from "../hooks/useURLposition";
+import { useCities } from "../contexts/CitiesContext";
+import { convertToEmoji } from "./ConvertToEmoji";
 
 import styles from "./Form.module.css";
-import { useCities } from "../contexts/CitiesContext";
 import BackButton from "./BackButton";
 import Spinner from "./Spinner";
 import Message from "./Message";
 import Button from "./Button";
-import { useNavigate } from "react-router-dom";
-
-export function convertToEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
-}
 
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
